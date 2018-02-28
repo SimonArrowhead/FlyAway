4e6a862336a11471af4bcb4f136d44e7f8b4f116
Simon Arrowhead <szymon.groth@gmail.com>
2018-02-28 23:44:01 +0100

    Button experiments

--- app/src/main/java/com/example/matrix/flyaway/MainActivity.java
+++ app/src/main/java/com/example/matrix/flyaway/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.matrix.flyaway;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
+
+    int number = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void count (View view){
+        display(number);
+        number++;
+    }
+
+    private void display(int testNumber){
+        TextView testTextView = (TextView) findViewById(R.id.test);
+        testTextView.setText(NumberFormat.getNumberInstance().format(number));
     }
 }
